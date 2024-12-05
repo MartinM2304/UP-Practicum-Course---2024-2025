30e66196a3fed6739e6816c8b3e7d48f1ba270b5
Slavi15 <slavirezashkiofficial@gmail.com>
2024-12-05 23:55:00 +0200

    Add Week10

---
+++ Week10/string_functions.cpp
@@ -0,0 +1,31 @@
+size_t myStrLen(const char* str)
+{
+	if (!str) return 0;
+
+	size_t result = 0;
+	while (*str++ && ++result);
+	return result;
+}
+
+void myStrCpy(char* dest, const char* src)
+{
+	if (!dest || !src) return;
+
+	while (*dest++ = *src++);
+}
+
+void myStrcat(char* first, const char* second)
+{
+	if (!first || !second) return;
+
+	first += myStrLen(first);
+	myStrCpy(first, second);
+}
+
+int myStrCmp(const char* first, const char* second)
+{
+	if (!first || !second) return 0;
+
+	while (*first && *first++ == *second++);
+	return *first - *second;
+}
